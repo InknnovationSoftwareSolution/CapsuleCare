166fa45bae88217e5601c4a35a1185979e343216
Melany MS <melamoralessoriano@gmail.com>
2024-07-12 19:23:19 -0600

    Pipes-Validation

--- src/shedules/shedelus.dto.ts
+++ src/shedules/shedelus.dto.ts
@@ -1,11 +1,26 @@
+import {  IsInt, IsNotEmpty } from 'class-validator';
+
 export class newShed{
+    @IsNotEmpty({message:"No puede quedar vacío"})
+    @IsInt({message:"El dato debe ser un número"})
     medicina: number
+
+    @IsNotEmpty({message:"No puede quedar vacío"})
+    @IsInt({message:"El dato debe ser un número"})
     user: number
+
+    @IsNotEmpty({message:"No puede quedar vacío"})
+    @IsInt({message:"El dato debe ser un número"})
     intervalo: number
+
+
     finish_time: Date
 }
 
 export class updatShed{
+    @IsNotEmpty({message:"No puede quedar vacío"})
+    @IsInt({message:"El dato debe ser un número"})
     intervalo?: number
+    
     finish_time?: Date
 }
