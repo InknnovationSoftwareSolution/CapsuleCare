40e7856d8f50bb99325a0a4063d0896ba799bbfd
Melany MS <melamoralessoriano@gmail.com>
2024-08-30 17:25:59 -0600

    cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,9 +14,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: '',
-      database: '',
+      username: 'process.env.DB_USER',
+      password: 'process.env.PASSWORD',
+      database: 'process.env.DB_NAME',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true
     }),
